986144eab6b37a2fae6ca29ddbffaa6698bffb1e
MarcieD1 <marciatarisai@icloud.com>
2024-06-06 17:45:04 +0200

    Fixing Pycodestyle and code

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
+'''
+type-annotated function
+'''
+import math
 
-   def floor(n: float) -> int:
-       return math.floor(n)
+
+def floor(n: float) -> int:
+    '''function that floor a float and return an integer
+    '''
+    return (math.floor(n))
