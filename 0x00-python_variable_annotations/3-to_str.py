d2a46e6039bd679bb01579d24ab5b8296151340c
MarcieD1 <marciatarisai@icloud.com>
2024-06-06 17:46:26 +0200

    Editing the indentations

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+'''
+type-annotated function
+'''
 
-   def to_str(n: float) -> str:
-       return str(n)
+
+def to_str(n: float) -> str:
+    '''converts float to strings
+    '''
+    return (str(n))
