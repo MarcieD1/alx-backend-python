e4ce0abe7030efa82e135ba025c974f20d4a6ed4
MarcieD1 <marciatarisai@icloud.com>
2024-06-10 19:40:37 +0200

    Fixing error bugs

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
-from typing import List
+'''Asyncronus python
+'''
 import asyncio
-from 0-basic_async_syntax import wait_random
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    delays = [await wait_random(max_delay) for _ in range(n)]
-    return sorted(delays)
+    '''return a list of awaited response from previous function
+    '''
+    res = await asyncio.gather(
+        *tuple(map(lambda _: wait_random(max_delay), range(n)))
+    )
+    return sorted(res)
